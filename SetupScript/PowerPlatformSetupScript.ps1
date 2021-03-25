a3139c1ac237ddb8e0dd0158c4012e8c7a202a00
svarukala <svarukala@gmail.com>
2021-03-25 14:03:53 -0400

    added new files

---
+++ SetupScript/PowerPlatformSetupScript.ps1
@@ -0,0 +1,24 @@
+﻿Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+
+Install-Module -Name Microsoft.PowerApps.Administration.PowerShell #-Force
+Install-Module -Name Microsoft.PowerApps.PowerShell -AllowClobber
+
+#Alternatively, if you don't have admin rights on your computer, you can use the following to use these modules:
+#Save-Module -Name Microsoft.PowerApps.Administration.PowerShell -Path
+#Import-Module -Name Microsoft.PowerApps.Administration.PowerShell
+#Save-Module -Name Microsoft.PowerApps.PowerShell -Path
+#Import-Module -Name Microsoft.PowerApps.PowerShell
+
+
+Set-PSRepository -Name 'PSGallery' -InstallationPolicy Untrusted
+
+# Here is how you can pass in credentials (avoiding opening a prompt)
+$pass = ConvertTo-SecureString "PES@201909" -AsPlainText -Force
+Add-PowerAppsAccount -Username MeganB@M365x229910.OnMicrosoft.com -Password $pass
+
+Get-FlowEnvironment | Select *
+
+Get-AdminPowerAppEnvironment
+
+#https://docs.microsoft.com/en-us/power-platform/admin/powerapps-powershell
+#https://docs.microsoft.com/en-us/power-automate/web-api
